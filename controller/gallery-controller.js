e77ffb727b8bcc5b8faca3446aef881c2e47d48c
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-12 13:04:48 -0500

    gallery controller done

---
+++ controller/gallery-controller.js
@@ -0,0 +1,61 @@
+const Gallery = require('../models/Gallery')
+
+const galleryController = {
+
+  index(req, res, next) {
+    Gallery.getAll()
+      .then((galleries) => {
+        res.render('galleries/index', {
+          message: 'ok',
+          data: {galleries}
+        })
+      })
+      .catch(next)
+  },
+
+  show(req, res, next) {
+    Gallery.getById(req.params.id)
+    .then((gallery) => {
+      res.locals.gallery = gallery
+      next()
+    })
+    .catch(next)
+  },
+
+  create(req, res, next) {
+    new Gallery({
+      name: req.body.name,
+      description: req.body.description,
+      user_id: req.user.id
+    })
+    .save()
+    .then(() => {
+      res.redirect('/galleries')
+    })
+    .catch(next)
+  },
+
+  update(req, res, next) {
+    Gallery.getById(req.params.id)
+    .then((gallery) => {
+      return gallery.update(req.body)
+    })
+    .then((updatedGallery) => {
+      res.redirect(`/galleries/${updatedGallery.id}`)
+    })
+    .catch(next)
+  },
+
+  delete(req, res, next) {
+    Gallery.getById(req.params.id)
+    .then((gallery) => {
+      return gallery.delete()
+    })
+    .then(() => {
+      res.redirect('/galleries')
+    })
+    .catch(next)
+  }
+}
+
+module.exports = galleryController
