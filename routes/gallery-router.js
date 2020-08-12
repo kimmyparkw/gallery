7e5cc528dc18fda4f2de01c780872a5a55dce654
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-12 12:54:25 -0500

    gallery router done

---
+++ routes/gallery-router.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const galleryRouter = express.Router()
+
+const galleryController = require('../contorller/gallery-controller')
+
+galleryRouter.get('/', galleryController.index)
+galleryRouter.post('/', galleryController.create)
+
+galleryController.get('/add', (req, res) => {
+  res.render('galleries/add')
+})
+
+galleryRouter.get('/:id([0-9]+)', galleryController.show, (req, res) => {
+  res.render('galleries/show', {
+    gallery: res.locals.gallery,
+  })
+})
+
+galleryRouter.get('/:id([0-9]+)/edit', galleryController.show, (req, res) => {
+  res.render('galleries/edit', {
+    gallery: res.locals.gallery,
+  })
+})
+
+galleryRouter.put('/:id', galleryController.update)
+
+galleryRouter.delete('/:id', galleryController.delete)
+
+module.exports = galleryRouter
