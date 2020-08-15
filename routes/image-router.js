fcc77f154337e7a060e3544f448bc11cfe1d0cc7
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-15 15:14:05 -0500

    images added

---
+++ routes/image-router.js
@@ -0,0 +1,14 @@
+const imageRouter = require('express').Router()
+
+const imagesController = require('../controller/images-controller')
+
+imageRouter.get('/', imagesController.index)
+
+imageRouter.get('/:id([0-9]+)', imagesController.show, (req, res) => {
+  res.render('images/show', {
+    image: res.locals.image
+  })
+})
+
+
+module.exports = imageRouter
