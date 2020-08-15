02e196cbe3a9e4e56e7c8babc193e687db436532
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-15 16:34:28 -0500

    main.js

--- public/src/main.js
+++ public/src/main.js
@@ -1,9 +1,11 @@
 const addImageEls = document.querySelectorAll('.add-image')
 const main = document.querySelector('.main')
-addImageEls.addEventListener('click', (el) => {
-  const addForm = document.createElement('form')
-  const addFormText = document.createElement('p')
-  p.innertext = 'Which gallery would you like to add to?'
-  main.append(addForm)
-  addForm.append(addFormText)
+addImageEls.forEach((image) => {
+  image.addEventListener('click', (el) => {
+    const addForm = document.createElement('form')
+    const addFormText = document.createElement('p')
+    p.innertext = 'Which gallery would you like to add to?'
+    main.append(addForm)
+    addForm.append(addFormText)
+  })
 })
