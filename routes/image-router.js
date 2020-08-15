1d9bfef72bba82fb0c54683659698bc7c92a98d6
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-15 15:20:54 -0500

    images

--- routes/image-router.js
+++ routes/image-router.js
@@ -2,9 +2,9 @@ const imageRouter = require('express').Router()
 
 const imageController = require('../controller/images-controller')
 
-imageRouter.get('/', imagesController.index)
+imageRouter.get('/', imageController.index)
 
-imageRouter.get('/:id([0-9]+)', imagesController.show, (req, res) => {
+imageRouter.get('/:id([0-9]+)', imageController.show, (req, res) => {
   res.render('images/show', {
     image: res.locals.image
   })
