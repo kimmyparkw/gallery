1af89940d4c20989fcf8b774344e70052e3cb359
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-17 08:43:11 -0500

    seed edits

--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -3,9 +3,6 @@ VALUES
 ('Nachtwacht, Schutters van wijk II onder leiding van kapitein Frans Banninck Cocq', 'Rembrandt van Rijn', 'https://lh3.googleusercontent.com/J-mxAE7CPu-DXIOx4QKBtb0GC4ud37da1QK7CzbTIDswmvZHXhLm4Tv2-1H3iBXJWAW_bHm7dMl3j5wv_XiWAg55VOM=s0'),
 ('Portret van een vrouw, mogelijk Maria Trip', 'Rembrandt van Rijn', 'https://lh3.googleusercontent.com/AyiKhdEWJ7XmtPXQbRg_kWqKn6mCV07bsuUB01hJHjVVP-ZQFmzjTWt7JIWiQFZbb9l5tKFhVOspmco4lMwqwWImfgg=s0'),
 ('Zelfportret als de apostel Paulus', 'Rembrandt van Rijn', 'https://lh3.googleusercontent.com/NrCcfeY0r2F3M2hIQe5SLDRofR2tVzeOH18VjflOYGj88v4clb4v2H_VgCZR4nJhYsxxH9ATzfkL2tRqOWEK5-gPVEE=s0');
-
-INSERT INTO images (title, artist, url)
-VALUES
 ('Christus aan het volk getoond: liggende plaat', 'Rembrandt van Rijn', 'https://lh5.ggpht.com/jrYBpTWXMcOU1swnC4KvgWGDdd-oyI6pmenHVagQijHiYburOxarx7Bu8MP06d7ceUzn4Mc6cD0EUeXgoGc8NwzJNMY=s0'),
 ('Marten Soolmans', 'Rembrandt van Rijn', 'https://lh3.googleusercontent.com/hDN8_JS4BbbksBXkxIDUldJvooejHBfQjgD3b_Jr73HjVOAs8thpnvCJA3WUtQFxlbjx-QB1-IyqhgEiM4dFLFk1Sg=s0'),
 ('Zelfportret', 'Rembrandt van Rijn', 'https://lh3.googleusercontent.com/7qzT0pbclLB7y3fdS1GxzMnV7m3gD3gWnhlquhFaJSn6gNOvMmTUAX3wVlTzhMXIs8kM9IH8AsjHNVTs8em3XQI6uMY=s0'),
